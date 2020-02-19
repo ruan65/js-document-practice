f8541920a0b0a3620bcbe76295d115ddbc00fa18
ruan65 <ruan65@gmail.com>
2020-02-20 00:28:35 +0300

    save

--- app.js
+++ app.js
@@ -35,3 +35,41 @@ hideBox.addEventListener('change', function(ev) {
     console.log('changed: ' + hideBox.checked);
     bookList.style.display = hideBox.checked ? 'none' : 'block';
 })
+
+const searchBar = document.forms['search-books'].querySelector('input');
+
+searchBar.addEventListener('keyup', function(ev) {
+
+    const term = ev.target.value.toLowerCase();
+
+    console.log(term)
+
+    const books = bookList.getElementsByTagName('li');
+    Array.from(books).forEach(function(book) {
+
+        const title = book.firstElementChild.textContent;
+
+        if(title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    });
+    
+});
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
